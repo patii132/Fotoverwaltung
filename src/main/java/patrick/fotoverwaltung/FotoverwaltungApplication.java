19b08cd2cbabb49d340c70488c7dabff58d1ec29
Patrick Schkorianz <P.Schkorianz@web.de>
2021-11-26 20:44:01 +0100

    first commit

---
+++ src/main/java/patrick/fotoverwaltung/FotoverwaltungApplication.java
@@ -0,0 +1,13 @@
+package patrick.fotoverwaltung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FotoverwaltungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FotoverwaltungApplication.class, args);
+	}
+
+}
---
+++ src/test/java/patrick/fotoverwaltung/FotoverwaltungApplicationTests.java
@@ -0,0 +1,13 @@
+package patrick.fotoverwaltung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FotoverwaltungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
